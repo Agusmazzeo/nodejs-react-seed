07b834f4c7ff4957671170b8f197c54df114dcbc
Agus Mazzeo <amazzeo@moorea.io>
2019-08-21 10:35:11 -0300

    Problema en archivo keys

--- back/config/keys.js
+++ back/config/keys.js
@@ -3,7 +3,3 @@ module.exports = {
   googleClientSecret: "_xAhn-RcLQxnbJ7w-x_KM2gc",
   cookieKey: "ñlkjhasdfghswertyu",
 };
-<<<<<<< HEAD
-=======
-
->>>>>>> 4fb8011fbea97c0cafcea1cdaf5a9c5c1663aab3
