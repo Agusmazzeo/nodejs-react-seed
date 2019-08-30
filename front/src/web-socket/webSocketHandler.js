427f0b5881464b1114e8f5c5afe925f6fe4cfc75
Agus Mazzeo <amazzeo@moorea.io>
2019-08-30 18:03:35 -0300

    Arreglado socket

--- front/src/web-socket/webSocketHandler.js
+++ front/src/web-socket/webSocketHandler.js
@@ -1,5 +1,5 @@
 import socketIOClient from "socket.io-client";
 
-// const socket = socketIOClient("http://167.71.167.77:3000");
-const socket = socketIOClient("http://localhost:3000");
+const socket = socketIOClient("http://167.71.167.77:3000");
+// const socket = socketIOClient("http://localhost:3000");
 export default socket;
